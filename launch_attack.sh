163d638a397a583f040adb2f65cd152dfb9b477a
Marco Garlet <garlet@testing.club.di.unimi.it>
2022-03-11 19:03:59 +0000

    rename main script

--- launch_attack.sh
+++ launch_attack.sh
@@ -5,7 +5,7 @@ do
     ./final_attack/offline/superpoly_rec/only_gpu/att1
     cat ./final_attack/offline/cubes_test_window.txt >> ./final_attack/offline/cubes_test.txt
     cat ./final_attack/offline/superpolies_window.txt >> ./final_attack/offline/superpolies.txt
-	if [[ $(wc -l <./final_attack/offline/cubes_test.txt) -ge 100 ]]; then
+	if [[ $(wc -l <./final_attack/offline/cubes_test.txt) -ge 10 ]]; then
 		break	
 	fi
 
